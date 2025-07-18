a0fdd8b122b6ce937e5528a37078549d10e7ac05
JordanLarson <jordanlarson14@gmail.com>
2025-07-18 15:06:09 -0700

    Add test main block

--- depth_anything_v2/dpt.py
+++ depth_anything_v2/dpt.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,7 +9,7 @@ from .dinov2 import DINOv2
 from .util.transform import Resize, NormalizeImage, PrepareForNet
 from .util.blocks import FeatureFusionBlock, _make_scratch
 
-
+# --- Utility Functions ---
 def _make_fusion_block(features, use_bn, size=None):
     return FeatureFusionBlock(
         features,
@@ -20,20 +21,56 @@ def _make_fusion_block(features, use_bn, size=None):
         size=size,
     )
 
+def extract_spatial_chunks(x, num_chunks_h, num_chunks_w):
+    # x: (B, C, H, W)
+    B, C, H, W = x.shape
+    kh = math.ceil(H / num_chunks_h)
+    kw = math.ceil(W / num_chunks_w)
+    tokens = []
+    for i in range(num_chunks_h):
+        for j in range(num_chunks_w):
+            h_start = i * kh
+            h_end = min((i + 1) * kh, H)
+            w_start = j * kw
+            w_end = min((j + 1) * kw, W)
+            chunk = x[:, :, h_start:h_end, w_start:w_end]  # (B, C, h_chunk, w_chunk)
+            pooled = F.adaptive_avg_pool2d(chunk, 1).view(B, C)  # (B, C)
+            tokens.append(pooled)
+    tokens = torch.stack(tokens, dim=1)  # (B, num_chunks_h * num_chunks_w, C)
+    return tokens
+
+
+class CrossAttentionBlock(nn.Module):
+    def __init__(self, embed_dim, num_heads=8):
+        super().__init__()
+        self.cross_attn = nn.MultiheadAttention(embed_dim, num_heads, batch_first=True)
+
+    def forward(self, query, key, value):
+        attn_out, _ = self.cross_attn(query, key, value)
+        return attn_out
+
+
+# --- DPT Head ---
+
 class DPTHead(nn.Module):
     def __init__(
-        self, 
-        in_channels, 
-        features=256, 
-        use_bn=False, 
-        out_channels=[256, 512, 1024, 1024], 
-        use_clstoken=False,
-        use_temporal_consistency=False,
-        num_temporal_tokens=1,
+            self,
+            in_channels,
+            features=256,
+            use_bn=False,
+            out_channels=[256, 512, 1024, 1024],
+            use_clstoken=False,
+            use_temporal_consistency=False,
+            num_spatial_tokens_h=2,  # number of spatial chunks along height
+            num_spatial_tokens_w=2,  # number of spatial chunks along width
+            cross_attn_heads=8,
     ):
         super(DPTHead, self).__init__()
-        
+
         self.use_clstoken = use_clstoken
+        self.use_temporal_consistency = use_temporal_consistency
+        self.num_spatial_tokens_h = num_spatial_tokens_h
+        self.num_spatial_tokens_w = num_spatial_tokens_w
 
         self.projects = nn.ModuleList([
             nn.Conv2d(
@@ -44,7 +81,7 @@ class DPTHead(nn.Module):
                 padding=0,
             ) for out_channel in out_channels
         ])
-        
+
         self.resize_layers = nn.ModuleList([
             nn.ConvTranspose2d(
                 in_channels=out_channels[0],
@@ -66,7 +103,7 @@ class DPTHead(nn.Module):
                 stride=2,
                 padding=1)
         ])
-        
+
         if use_clstoken:
             self.readout_projects = nn.ModuleList()
             for _ in range(len(self.projects)):
@@ -74,24 +111,24 @@ class DPTHead(nn.Module):
                     nn.Sequential(
                         nn.Linear(2 * in_channels, in_channels),
                         nn.GELU()))
-        
+
         self.scratch = _make_scratch(
             out_channels,
             features,
             groups=1,
             expand=False,
         )
-        
+
         self.scratch.stem_transpose = None
-        
+
         self.scratch.refinenet1 = _make_fusion_block(features, use_bn)
         self.scratch.refinenet2 = _make_fusion_block(features, use_bn)
         self.scratch.refinenet3 = _make_fusion_block(features, use_bn)
         self.scratch.refinenet4 = _make_fusion_block(features, use_bn)
-        
+
         head_features_1 = features
         head_features_2 = 32
-        
+
         self.scratch.output_conv1 = nn.Conv2d(head_features_1, head_features_1 // 2, kernel_size=3, stride=1, padding=1)
         self.scratch.output_conv2 = nn.Sequential(
             nn.Conv2d(head_features_1 // 2, head_features_2, kernel_size=3, stride=1, padding=1),
@@ -101,147 +138,132 @@ class DPTHead(nn.Module):
             nn.Identity(),
         )
 
-        # temporal consistency tokens
-        self.use_temporal_consistency = use_temporal_consistency
-        if self.use_temporal_consistency:
-            self.num_temporal_tokens = num_temporal_tokens
+        # Cross-attention for temporal consistency
+        if use_temporal_consistency:
+            self.cross_attn_block = CrossAttentionBlock(embed_dim=in_channels, num_heads=cross_attn_heads)
 
-            # how many patches each temporal token gets
-            num_temporal_patch_channels = in_channels // num_temporal_tokens
-
-            self.temporal_token_extraction = nn.ModuleList()
-            for i in range(self.num_temporal_tokens):
-                self.temporal_token_extraction.append(
-                    nn.Sequential(
-                        nn.Conv2d(num_temporal_patch_channels, 1, kernel_size=1, stride=1, padding=0),
-                        nn.ReLU(True),
-                    )
-                )
-
-
-    
-    def forward(self, out_features, patch_h, patch_w):
+    def forward(self, out_features, patch_h, patch_w, previous_temporal_tokens=None,):
         out = []
         for i, x in enumerate(out_features):
             if self.use_clstoken:
                 x, cls_token = x[0], x[1]
-                # create readout which is a copy of the cls token for each spatial token
-                readout = cls_token.unsqueeze(1).expand_as(x)
-                x = self.readout_projects[i](torch.cat((x, readout), -1))
             else:
                 x = x[0]
+                cls_token = None
 
+            # Extract spatial tokens from last feature map layer
+            if self.use_temporal_consistency and i == len(out_features) - 1:
+                spatial_tokens = extract_spatial_chunks(
+                    x,
+                    num_chunks_h=self.num_spatial_tokens_h,
+                    num_chunks_w=self.num_spatial_tokens_w
+                )  # (B, num_spatial_chunks, C)
+
+                B, C, H, W = x.shape
+                patch_tokens = x.view(B, C, -1).permute(0, 2, 1)  # (B, H*W, C)
+                token_list = [patch_tokens, spatial_tokens]
+                if self.use_clstoken and cls_token is not None:
+                    token_list.insert(0, cls_token.unsqueeze(1))  # (B, 1, C)
+                query = torch.cat(token_list, dim=1)  # (B, N_query, C)
+
+                if previous_temporal_tokens is not None:
+                    # previous_temporal_tokens: (B, N_temporal, C)
+                    # Use as keys and values
+                    attended = self.cross_attn_block(
+                        query=query,
+                        key=previous_temporal_tokens,
+                        value=previous_temporal_tokens
+                    )  # (B, N_query, C)
+
+                    # fuse attended output with original query (residual)
+                    query = query + attended
+
+                # Extract tokens
+                N_cls = 1 if self.use_clstoken and cls_token is not None else 0
+                N_patches = H * W
+
+                class_token_out = None
+                if N_cls == 1:
+                    class_token_out = query[:, 0, :]  # (B, C)
+                patch_tokens_out = query[:, N_cls:N_cls + N_patches, :]  # (B, H*W, C)
+                temporal_tokens_out = query[:, N_cls + N_patches:, :]  # (B, N_spatial, C)
+                temporal_tokens_out = temporal_tokens_out.permute(0, 2, 1) #(B, C, N_spatial)
+
+                temporal_class_tokens_out = torch.cat((class_token_out, temporal_tokens_out), dim=1)
+                x = patch_tokens_out.permute(0, 2, 1).reshape(B, C, H, W)
+
+            else:
+                readout = cls_token.unsqueeze(1).expand_as(x)
+                x = self.readout_projects[i](torch.cat((x, readout), -1))
+
+            # reassemble blocks
             x = x.permute(0, 2, 1).reshape((x.shape[0], x.shape[-1], patch_h, patch_w))
-            
             x = self.projects[i](x)
             x = self.resize_layers[i](x)
-            
             out.append(x)
-        
-        layer_1, layer_2, layer_3, layer_4 = out
 
-        if self.use_temporal_consistency:
-            B, C, H, W = layer_4.shape
-            assert H >= self.num_temporal_tokens, "Cannot have more temporal tokens than patch rows"
+        layer_1, layer_2, layer_3, layer_4 = out  # (B, C, H, W)
 
-            # Divide into `n` vertical chunks (along height axis)
-            chunks = torch.chunk(layer_4, self.num_temporal_tokens, dim=2)  # list of (B, C, H_chunk, W)
-
-            temporal_tokens = []
-            for i, chunk in enumerate(chunks):
-                # Pool over spatial dimensions to get one token per chunk
-                temporal_out = self.temporal_token_extraction[i](chunk)
-                token = temporal_out.view(B, C)  # (B, C)
-                temporal_tokens.append(token)
-
-            # Stack: (B, n, C)
-            temporal_tokens = torch.stack(temporal_tokens, dim=1)
-        # todo: no need to cast temporal tokens onto spatial tokens, the cross-attention block next will do that.
-        #Need to add the N frames of temporal tokens as an input to this forward call
-
+        # --- Standard DPT Head fusion/decoding ---
         layer_1_rn = self.scratch.layer1_rn(layer_1)
         layer_2_rn = self.scratch.layer2_rn(layer_2)
         layer_3_rn = self.scratch.layer3_rn(layer_3)
         layer_4_rn = self.scratch.layer4_rn(layer_4)
-        
-        path_4 = self.scratch.refinenet4(layer_4_rn, size=layer_3_rn.shape[2:])        
+
+        path_4 = self.scratch.refinenet4(layer_4_rn, size=layer_3_rn.shape[2:])
         path_3 = self.scratch.refinenet3(path_4, layer_3_rn, size=layer_2_rn.shape[2:])
         path_2 = self.scratch.refinenet2(path_3, layer_2_rn, size=layer_1_rn.shape[2:])
         path_1 = self.scratch.refinenet1(path_2, layer_1_rn)
-        
+
         out = self.scratch.output_conv1(path_1)
         out = F.interpolate(out, (int(patch_h * 14), int(patch_w * 14)), mode="bilinear", align_corners=True)
         out = self.scratch.output_conv2(out)
 
-        return out
+        return out, temporal_class_tokens_out
 
 
-class DepthAnythingV2(nn.Module):
-    def __init__(
-        self, 
-        encoder='vitl', 
-        features=256, 
-        out_channels=[256, 512, 1024, 1024], 
-        use_bn=False, 
-        use_clstoken=False
-    ):
-        super(DepthAnythingV2, self).__init__()
-        
-        self.intermediate_layer_idx = {
-            'vits': [2, 5, 8, 11],
-            'vitb': [2, 5, 8, 11], 
-            'vitl': [4, 11, 17, 23], 
-            'vitg': [9, 19, 29, 39]
-        }
-        
-        self.encoder = encoder
-        self.pretrained = DINOv2(model_name=encoder)
-        
-        self.depth_head = DPTHead(self.pretrained.embed_dim, features, use_bn, out_channels=out_channels, use_clstoken=use_clstoken)
-    
-    def forward(self, x):
-        patch_h, patch_w = x.shape[-2] // 14, x.shape[-1] // 14
-        
-        features = self.pretrained.get_intermediate_layers(x, self.intermediate_layer_idx[self.encoder], return_class_token=True)
-        
-        depth = self.depth_head(features, patch_h, patch_w)
-        depth = F.relu(depth)
-        
-        return depth.squeeze(1)
-    
-    @torch.no_grad()
-    def infer_image(self, raw_image, input_size=518):
-        image, (h, w) = self.image2tensor(raw_image, input_size)
-        
-        depth = self.forward(image)
-        
-        depth = F.interpolate(depth[:, None], (h, w), mode="bilinear", align_corners=True)[0, 0]
-        
-        return depth.cpu().numpy()
-    
-    def image2tensor(self, raw_image, input_size=518):        
-        transform = Compose([
-            Resize(
-                width=input_size,
-                height=input_size,
-                resize_target=False,
-                keep_aspect_ratio=True,
-                ensure_multiple_of=14,
-                resize_method='lower_bound',
-                image_interpolation_method=cv2.INTER_CUBIC,
-            ),
-            NormalizeImage(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            PrepareForNet(),
-        ])
-        
-        h, w = raw_image.shape[:2]
-        
-        image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB) / 255.0
-        
-        image = transform({'image': image})['image']
-        image = torch.from_numpy(image).unsqueeze(0)
-        
-        DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
-        image = image.to(DEVICE)
-        
-        return image, (h, w)
+def main():
+    B = 2                # batch size
+    C = 256              # input channel size
+    patch_h = patch_w = 12
+    num_layers = 4
+    out_channels = [256, 512, 1024, 1024]
+
+    # Create fake intermediate ViT outputs for 4 layers
+    # Format: [(tokens, cls_token), ...]
+    # tokens: (B, N, C) where N = H*W, cls_token: (B, C)
+    out_features = []
+    for l in range(num_layers):
+        H = patch_h // (2 ** (num_layers - l - 1))
+        W = patch_w // (2 ** (num_layers - l - 1))
+        N = H * W
+        tokens = torch.randn(B, N, C)
+        cls_token = torch.randn(B, C)
+        out_features.append((tokens, cls_token))
+
+    # Fake previous_temporal_tokens: (B, N_temporal, C)
+    N_temporal = 8
+    previous_temporal_tokens = torch.randn(B, N_temporal, C)
+
+    # Instantiate head
+    dpt_head = DPTHead(
+        in_channels=C,
+        use_clstoken=True,
+        use_temporal_consistency=True,
+        num_spatial_tokens_h=2,
+        num_spatial_tokens_w=2
+    )
+
+    # Run head
+    out, temporal_class_tokens_out = dpt_head(
+        out_features,
+        patch_h=patch_h,
+        patch_w=patch_w,
+        previous_temporal_tokens=previous_temporal_tokens,
+    )
+
+    print(f"Output depth shape: {out.shape}")
+    print(f"Temporal+class tokens shape: {temporal_class_tokens_out.shape}")
+
+if __name__ == '__main__':
+    main()
