568f9a09bae3110fe6d782c5c07a1cd1991a7342
JordanLarson <jordanlarson14@gmail.com>
2025-12-07 19:53:22 -0800

    fix DPT layer issue, water through pipes, should work now

--- ml4ded/models/dino2seg.py
+++ ml4ded/models/dino2seg.py
@@ -268,10 +268,10 @@ class DPTSegmentationHead(nn.Module):
         layer_1, layer_2, layer_3, layer_4 = out
 
         # --- Standard DPT Fusion ---
-        layer_1_rn = self.scratch.refinenet1(layer_1)
-        layer_2_rn = self.scratch.refinenet2(layer_2)
-        layer_3_rn = self.scratch.refinenet3(layer_3)
-        layer_4_rn = self.scratch.refinenet4(layer_4)
+        layer_1_rn = self.scratch.layer1_rn(layer_1)
+        layer_2_rn = self.scratch.layer2_rn(layer_2)
+        layer_3_rn = self.scratch.layer3_rn(layer_3)
+        layer_4_rn = self.scratch.layer4_rn(layer_4)
 
         path_4 = self.scratch.refinenet4(layer_4_rn, size=layer_3_rn.shape[2:])
         path_3 = self.scratch.refinenet3(path_4, layer_3_rn, size=layer_2_rn.shape[2:])
